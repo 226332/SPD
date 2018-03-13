b27f865c9dce6aa6c2180842c01a30d501351f88
Your Name <you@example.com>
2018-03-13 19:30:53 +0100

    dodane 2opt

--- src/Scheduler.cpp
+++ src/Scheduler.cpp
@@ -1,4 +1,5 @@
 #include "../inc/Scheduler.hpp"
+#include "../inc/Cmax.hpp"
 Tasks SortR::schedule(std::vector<Task> tasks) {
     std::sort(tasks.begin(), tasks.end(), [](Task & task1, Task & task2)->bool {return task1.r<task2.r;});
     //for_each(tasks.begin(), tasks.end(), [](Task & task) {std::cout << task.r <<" ";}); //debug print
@@ -7,5 +8,26 @@ Tasks SortR::schedule(std::vector<Task> tasks) {
 
 Tasks Opt2::schedule(std::vector<Task> tasks) {
     //TODO: implement Opt2.
+	unsigned int best;
+	best = calculateCmax(tasks);
+	unsigned int k, i;
+	k = 0;
+	i = 1;
+	while (k<(tasks.size()-1)) {
+		std::iter_swap(tasks.begin()+k, tasks.begin()+i);
+		if (calculateCmax(tasks) >= best) {
+			std::iter_swap(tasks.begin()+k, tasks.begin()+i);
+			if (i == (tasks.size() - 1)) {
+				k++;
+				i = k+1;
+			}
+			else i++;
+		}
+		else {
+			best = calculateCmax(tasks);
+			k = 0;
+			i = 1;
+		}
+	}
     return tasks;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,10 @@
 
 int main() {
     SortR scheduler;
-    Tasks tasks = readTaskFile(std::string("pliki/in10.txt"));
+	Opt2 scheduler2;
+    Tasks tasks = readTaskFile(std::string("pliki/in100_3.txt"));
     std::cout << "SortR: " << calculateCmax(scheduler.schedule(tasks)) << std::endl;
     std::cout << "Unscheduled: : " << calculateCmax(tasks) << std::endl;
+	std::cout << "Opt2: " << calculateCmax(scheduler2.schedule(tasks)) << std::endl;
+	std::cout << "SortR+Opt2: " << calculateCmax(scheduler2.schedule(scheduler.schedule(tasks))) << std::endl;
 }
